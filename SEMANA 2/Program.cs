7725fc811df7999c4348e07934208a9adfd0c86c
SavioNaguicha <savionaguicha@hotmail.com>
2026-01-10 04:46:52 -0500

    Mi primer commit de C#

---
+++ SEMANA 2/Program.cs
@@ -0,0 +1,32 @@
+﻿//CalcularArea es una función que devuelve un valor double, se utiliza para calcular el área de un circulo, requiere 
+//como argumento el radio del circulo
+
+using System;
+
+namespace CalcularArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio = 5.0;
+            double calcular =  CalcularArea(radio);
+
+            //Se muestra el resulatdo en la consola
+            Console. WriteLine($" EL area del cicuito con radio {radio} es: {calcular:f4}");
+            Console.ReadKey();
+        }
+
+            //
+            public static double CalcularArea(double Radio)
+            {
+                return Math.PI*Radio * Radio;
+                
+
+
+        }
+    }
+}
+
+        
+    
