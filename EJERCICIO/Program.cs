a7b426397be93d4b926e05c8209745d160a0726e
SavioNaguicha <savionaguicha@hotmail.com>
2026-01-11 23:44:33 -0500

    EJERCICIO
    EJERCICIO

---
+++ EJERCICIO/Program.cs
@@ -0,0 +1,33 @@
+﻿
+//Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+using System;
+namespace Savio
+{
+    class Program
+    {
+        static void Main(string[] srgs)
+        {
+            int mayordeEdad = 35;
+
+
+            if(mayordeEdad > 35);
+            {
+                Console.Write("Mentira: ");
+            }
+            if(mayordeEdad==35)
+            {
+                Console.WriteLine("Eres mayor de edad");
+                {
+                    if(mayordeEdad < 35);
+                    {
+                        Console.WriteLine("adulto");
+                        {
+                            Console.Read();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
